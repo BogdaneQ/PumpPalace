81421ad3391bc3fcaaff1d4d6bb2434b0c0049f5
BogdaneQ <mati_bo@op.pl>
2025-01-05 17:02:02 +0100

    XDD

---
+++ Models/ViewsCounter.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PumpPalace.Models
+{
+    public class ViewsCounter
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? Counter { get; set; }
+    }
+}
